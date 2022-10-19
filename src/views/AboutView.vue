1dbf4558cc3f6e71959caa597d9b32cd8a0130c6
jordy muniz <jordy.muniz96@gmail.com>
2022-10-19 15:07:41 -0500

    made my first vue project

---
+++ src/views/AboutView.vue
@@ -0,0 +1,16 @@
+<script setup>
+let name = "jordy";
+</script>
+
+<template>
+  <h1>Hello, {{name}}</h1>
+</template>
+<style>
+@media (min-width: 1024px) {
+  .about {
+    min-height: 100vh;
+    display: flex;
+    align-items: center;
+  }
+}
+</style>
