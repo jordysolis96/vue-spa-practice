a39e739dfe81521b68f755fdaccd917265f65a8f
jordy muniz <jordy.muniz96@gmail.com>
2022-10-21 14:15:03 -0500

    building rock paper scissors

--- src/App.vue
+++ src/App.vue
@@ -1,3 +1,4 @@
+<!--The content here will appear on every page-->
 <script setup>
 import { RouterLink, RouterView } from 'vue-router'
 import TopNav from './components/TopNav.vue'
@@ -5,31 +6,44 @@ let user = {
   id: 922022,
   username: "CodeyThaDuck",
   password: "gocodeup",
+  themePreference: "dark-theme",
   email: "codey@codeup.com"
 }
+
+let preference = user.themePreference;
 </script>
 
 <template>
-
+<!--  <html v-bind:class="preference">-->
   <TopNav v-bind:user-data="user" />
 
-<!--      <nav>-->
-<!--        <RouterLink to="/">Home</RouterLink>-->
-<!--        <RouterLink to="/about">About</RouterLink>-->
-<!--        <RouterLink to="/exercise">Exercise</RouterLink>-->
-<!--        <RouterLink to="/demo">demo</RouterLink>-->
-<!--        <RouterLink to="/rps">RPS</RouterLink>-->
-<!--        <RouterLink to="/products">products</RouterLink>-->
-<!--        <RouterLink to="/checkout">Checkout</RouterLink>-->
-<!--        <RouterLink to="/inventory">Inventory</RouterLink>-->
-
-
-<!--      </nav>-->
-
-
   <RouterView />
+
+<!--  <main class="container d-flex justify-content-center">-->
+<!--    &lt;!&ndash;references the value of our 'preference' variable&ndash;&gt;-->
+<!--    <form v-bind:class="preference" class="w-50 p-3">-->
+<!--      <h2>Login</h2>-->
+<!--      <label for="username" class="form-label">Username</label>-->
+<!--      <input type="text" id="username" name="username" class="form-control">-->
+<!--      <label for="password" class="form-label">Password</label>-->
+<!--      <input type="password" id="password" name="password" class="form-control">-->
+<!--      <div class="text-center mt-3">-->
+<!--        &lt;!&ndash;references the value of our 'preference' variable (shorthand)&ndash;&gt;-->
+<!--        <button :class="preference" class="w-50" type="submit">Login</button>-->
+<!--      </div>-->
+<!--    </form>-->
+<!--  </main>-->
+<!--  </html>-->
 </template>
 
 <style scoped>
+.dark-theme {
+  background-color: #020403; /* Obsidian black */
+  color: whitesmoke;
+}
 
+.hufflepuff-theme {
+  background-color: goldenrod;
+  color: #020403; /* Obsidian black */
+}
 </style>
--- src/views/RPSView.vue
+++ src/views/RPSView.vue
@@ -1,3 +1,30 @@
+<script setup>
+let choices = ["rock", "paper", "scissors"];
+let choiceIndex = Math.floor((Math.random() * 3));
+let player1 = choices[choiceIndex];
+function playerChoice() {
+  let choice = document.getElementById("choice");
+  let text = choice.options[choice.selectedIndex].text;
+  document.getElementById("userChoice").innerHTML = text;
+}
+
+
+</script>
 <template>
-  <h1>This is the RPS page</h1>
+  <h1 class="row justify-content-center">Lets play Rock, Paper, Scissors {{ player1 }}</h1>
+  <form class="row g-3 justify-content-center">
+    <div class="col-4">
+      <select class="form-select form-select-sm" aria-label=".form-select-sm example" id="choice">
+        <option selected class="text center">select rock, paper, or scissor</option>
+        <option value="1">Rock</option>
+        <option value="2">Paper</option>
+        <option value="3">Scissors</option>
+      </select>
+    </div>
+    <div class="col-4">
+      <button type="button" class="btn btn-dark w-100" id="submit-btn" v-on:click="playerChoice">Let's play!</button>
+    </div>
+  </form>
+  <h1 id="userChoice"></h1>
+  <p id="timer"></p>
 </template>
