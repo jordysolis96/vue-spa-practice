79af64ba4d118be60306cf31a505b495917e7e05
jordy muniz <jordy.muniz96@gmail.com>
2022-10-24 12:35:24 -0500

    iterated through JSON file using v-for

--- src/components/ProductCard.vue
+++ src/components/ProductCard.vue
@@ -1,18 +1,19 @@
 <script setup>
 const props = defineProps(['productData'])
+
 </script>
 
 <template>
-  <div class="card mb-3" style="max-width: 540px;">
+  <div class="card mb-3" style="max-width: 540px;" v-for="product in productData" :key="product.id">
     <div class="row g-0">
       <div class="col-md-4">
-        <img v-bind:src="productData.image" class="img-fluid rounded-start" alt="...">
+        <img v-bind:src="product.image" class="img-fluid rounded-start" alt="...">
       </div>
       <div class="col-md-8">
         <div class="card-body">
-          <h5 class="card-title">{{ productData.title }}</h5>
-          <p class="card-text">{{ productData.description }}</p>
-          <p class="card-text"><small class="text-muted">{{ productData.price }}</small></p>
+          <h5 class="card-title">{{ product.title }}</h5>
+          <p class="card-text">{{ product.description }}</p>
+          <p class="card-text"><small class="text-muted">{{ product.price }}</small></p>
         </div>
       </div>
     </div>
--- src/views/ProductsView.vue
+++ src/views/ProductsView.vue
@@ -1,13 +1,18 @@
 <script setup>
-import ProductCard from "../components/ProductCard.vue"
+import {products} from "../data/products.js";
+import ProductCard from "../components/ProductCard.vue";
+
 let product = {
   title: "berserk vol.1",
   description: "This ultraviolent fantasy manga is the first in a series about a cursed lone wanderer with a tragic fate and a cast of bio-horror demons.",
   price: "$49.99",
   image: "https://m.media-amazon.com/images/I/513rmAmf1CL._SX218_BO1,204,203,200_QL40_FMwebp_.jpg"
 }
+
+console.log(products);
 </script>
 <template>
   <h1>This is the product page</h1>
-  <ProductCard v-bind:product-data="product"/>
+<!--  <ProductCard v-bind:product-data="product"/>-->
+  <ProductCard v-bind:product-data="products"/>
 </template>
